cbdff4d33563ae86a2e77873dc9db9264f6b875b
Compulink-Dev <devcompulink@gmail.com>
2024-04-29 10:58:34 +0200

    main

--- app/(main)/checkout/page.tsx
+++ app/(main)/checkout/page.tsx
@@ -14,7 +14,7 @@ interface PaymentProps {
 const PayNowCard = () => {
     return (
         <a href='https://www.paynow.co.zw/Payment/Link/?q=c2VhcmNoPWRpZ2l0YWxwYXltZW50cyU0MGNvbXB1bGluay5jby56dyZhbW91bnQ9NDAwLjAwJnJlZmVyZW5jZT1jb21wdTEyMyZsPTE%3d' target='_blank'>
-            <Image src='https://www.paynow.co.zw/Content/Buttons/Medium_buttons/button_pay-now_medium.png' style={{ border: 0 }} alt='' />
+            <Image width={100} height={100} src='https://www.paynow.co.zw/Content/Buttons/Medium_buttons/button_pay-now_medium.png' style={{ border: 0 }} alt='' />
         </a>
     )
 }
@@ -23,15 +23,13 @@ const PayNowCard = () => {
 const PaymentCard = ({ title, description, price }: PaymentProps) => {
     return (
         <div className="">
-            <a href={'https://www.paynow.co.zw/compu'} target="_blank" rel="noopener noreferrer" className=''>
-                <div className='border-2 border-blue-950 bg-white rounded h-[400px] w-[300px] hover:shadow-4xl'>
-                    <Image src={'/image.png'} alt='' width={100} height={100} className='w-full' />
-                    <div className="">
-                        <p className='text-center text-2xl font-bold text-blue-800'>{title}</p>
-                        <p className='text-center mt-2 font-bold text-blue-800 text-lg'>{price}</p>
-                    </div>
+            <div className='border-2 border-blue-950 bg-white rounded h-[400px] w-[300px] hover:shadow-4xl'>
+                <Image src={'/image.png'} alt='' width={100} height={100} className='w-full' />
+                <div className="">
+                    <p className='text-center text-2xl font-bold text-blue-800'>{title}</p>
+                    <p className='text-center mt-2 font-bold text-blue-800 text-lg'>{price}</p>
                 </div>
-            </a>
+            </div>
         </div>
     )
 }
@@ -43,9 +41,12 @@ function Checkout() {
                 <p className="mb-4 text-lg text-white font-bold">Select your package and join the Evolve Summit</p>
                 {/* <SignOutButton /> */}
                 <div className="flex items-center justify-center gap-8">
-                    <PayNowCard />
-                    <PaymentCard title={'Student'} price={'$200'} />
-                    <PaymentCard title={'Delegates'} price={'$400'} />
+                    <a href='https://www.paynow.co.zw/Payment/Link/?q=c2VhcmNoPWRpZ2l0YWxwYXltZW50cyU0MGNvbXB1bGluay5jby56dyZhbW91bnQ9MjAwLjAwJnJlZmVyZW5jZT0mbD0x' target='_blank'>
+                        <PaymentCard title={'Students'} price={'$200'} />
+                    </a>
+                    <a href='https://www.paynow.co.zw/Payment/Link/?q=c2VhcmNoPWRpZ2l0YWxwYXltZW50cyU0MGNvbXB1bGluay5jby56dyZhbW91bnQ9NDAwLjAwJnJlZmVyZW5jZT1EZWxlZ2F0ZTIwMjQmbD0x' target='_blank'>
+                        <PaymentCard title={'Delegates'} price={'$400'} />
+                    </a>
                 </div>
             </div>
 
