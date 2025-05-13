0d4d89955cd452b435a3b5b7ad8dbfd5216895ec
Compulink-Dev <devcompulink@gmail.com>
2025-05-13 10:35:13 +0200

    registration

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,136 +1,122 @@
-
-import React from 'react'
-import DashboardCard from './_components/DashboardCard'
-import TotalForm from './_components/TotalForm'
-import AnnouncementForm from './_components/AnnouncementForm'
-import ActivityForm from './_components/ActivityForm'
-import ScheduleForm from './_components/ScheduleForm'
-import Link from 'next/link'
-
+import React from "react";
+import DashboardCard from "./_components/DashboardCard";
+import TotalForm from "./_components/TotalForm";
+import AnnouncementForm from "./_components/AnnouncementForm";
+import ActivityForm from "./_components/ActivityForm";
+import ScheduleForm from "./_components/ScheduleForm";
+import Link from "next/link";
 
 interface VisitorProps {
-    name: string
-    surname: string
-    position: string
-    company: string
-    status: string
-    number: string
-    country: string
-    email: string
-    industry: string
+  name: string;
+  surname: string;
+  position: string;
+  company: string;
+  status: string;
+  number: string;
+  country: string;
+  email: string;
+  industry: string;
 }
 
 interface CardProps {
-    title: string
-    figure: number
-    subtitle: string
+  title: string;
+  figure: number;
+  subtitle: string;
 }
-
-
 
 const getVisitors = async () => {
-    try {
-        const res = await fetch(`${process.env.API_ROUTE}/api/registration/`, {
-            cache: 'no-store'
-        })
+  try {
+    const res = await fetch(`${process.env.API_ROUTE}/api/registration/`, {});
 
-        if (!res.ok) {
-            throw new Error("Failed to fetch visitors")
-        }
-        return res.json()
-    } catch (error) {
-        console.log("Error loading visitors", error);
-
+    if (!res.ok) {
+      throw new Error("Failed to fetch visitors");
     }
-}
+    return res.json();
+  } catch (error) {
+    console.log("Error loading visitors", error);
+  }
+};
 
 const VisitorCard = ({ title, figure, subtitle }: CardProps) => {
-    return (
-        <Link
-            href={'/dashboard/visitors'}
-            className='h-36 w-full bg-blue-300 p-4 rounded-lg flex flex-col items-start justify-between'>
-            <p className="text-sm md:text-md font-bold">{title}</p>
-            <p className="text-lg md:text-4xl font-bold">{figure}</p>
-            <p className="text-xs md:text-sm">{subtitle}</p>
-        </Link>
-    )
-}
-
+  return (
+    <Link
+      href={"/dashboard/visitors"}
+      className="h-36 w-full bg-blue-300 p-4 rounded-lg flex flex-col items-start justify-between"
+    >
+      <p className="text-sm md:text-md font-bold">{title}</p>
+      <p className="text-lg md:text-4xl font-bold">{figure}</p>
+      <p className="text-xs md:text-sm">{subtitle}</p>
+    </Link>
+  );
+};
 
 async function Dashboard() {
-    const { visitors } = await getVisitors()
-    let number = 0
+  const { visitors } = await getVisitors();
+  let number = 0;
 
-    return (
-        <div className='mt-4'>
-            <div className="">
-                {
-                    visitors?.map((data: any, index: any) => {
-                        number = index
-                        return (
-                            <div key={index} className="hidden">{index}
-                            </div>
-                        )
-                    })
-                }
+  return (
+    <div className="mt-4">
+      <div className="">
+        {visitors?.map((data: any, index: any) => {
+          number = index;
+          return (
+            <div key={index} className="hidden">
+              {index}
             </div>
-            <div className="flex flex-col md:flex-row gap-2">
-                <div className="" style={{ flex: 4 }}>
-                    <div className="flex flex-col md:flex-row gap-4">
-                        <VisitorCard
-                            title='Visitors'
-                            figure={number}
-                            subtitle='Confirmed visitors'
-                        />
-                        <DashboardCard
-                            title='Visitors'
-                            figure={10}
-                            subtitle='Pending confirmation'
-                        />
-                        <DashboardCard
-                            title='Visitors'
-                            figure={2}
-                            subtitle='Awaiting approval'
-                        />
-                    </div>
-                    <div className="mt-4 flex gap-4">
-                        <TotalForm
-                            title='Total Sponsors'
-                            figure={12}
-                            progress={12}
-                            gender='12 Male'
-                            gender2='20 Fe,male'
-                            average='2% per month'
-                        />
-                        <TotalForm
-                            title='Total Visitors'
-                            figure={number}
-                            progress={number}
-                            gender='12 Male'
-                            gender2='20 Fe,male'
-                            average='2% per month'
-                        />
-                    </div>
-                    <div className="my-4">
-                        <AnnouncementForm />
-                    </div>
-                </div>
-
-                <div className="" style={{ flex: 2 }}>
-                    <ActivityForm />
-                    <div className="my-4">
-                        <ScheduleForm />
-                    </div>
-                </div>
-
-
-
-            </div>
-
-
-
+          );
+        })}
+      </div>
+      <div className="flex flex-col md:flex-row gap-2">
+        <div className="" style={{ flex: 4 }}>
+          <div className="flex flex-col md:flex-row gap-4">
+            <VisitorCard
+              title="Visitors"
+              figure={number}
+              subtitle="Confirmed visitors"
+            />
+            <DashboardCard
+              title="Visitors"
+              figure={10}
+              subtitle="Pending confirmation"
+            />
+            <DashboardCard
+              title="Visitors"
+              figure={2}
+              subtitle="Awaiting approval"
+            />
+          </div>
+          <div className="mt-4 flex gap-4">
+            <TotalForm
+              title="Total Sponsors"
+              figure={12}
+              progress={12}
+              gender="12 Male"
+              gender2="20 Fe,male"
+              average="2% per month"
+            />
+            <TotalForm
+              title="Total Visitors"
+              figure={number}
+              progress={number}
+              gender="12 Male"
+              gender2="20 Fe,male"
+              average="2% per month"
+            />
+          </div>
+          <div className="my-4">
+            <AnnouncementForm />
+          </div>
         </div>
-    )
+
+        <div className="" style={{ flex: 2 }}>
+          <ActivityForm />
+          <div className="my-4">
+            <ScheduleForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- app/(admin)/dashboard/visitors/page.tsx
+++ app/(admin)/dashboard/visitors/page.tsx
@@ -1,136 +1,152 @@
-import React from 'react'
-import { MdDelete, MdEdit, MdEmail, MdFlag, MdHomeWork, MdLocationPin, MdPerson, MdPhone, MdUpdate } from 'react-icons/md'
+import React from "react";
+import {
+  MdDelete,
+  MdEdit,
+  MdEmail,
+  MdFlag,
+  MdHomeWork,
+  MdLocationPin,
+  MdPerson,
+  MdPhone,
+  MdUpdate,
+} from "react-icons/md";
 
 interface VisitorProps {
-    name: string
-    surname: string
-    position: string
-    company: string
-    status: string
-    number: string
-    country: string
-    email: string
-    industry: string
-    state: 'Student' | 'Company'
+  name: string;
+  surname: string;
+  position: string;
+  company: string;
+  status: string;
+  number: string;
+  country: string;
+  email: string;
+  industry: string;
+  state: "Student" | "Company";
 }
-
 
 const getVisitors = async () => {
-    try {
-        const res = await fetch(`${process.env.API_ROUTE}/api/registration/`, {
-            cache: 'no-store'
-        })
+  try {
+    const res = await fetch(`${process.env.API_ROUTE}/api/registration/`, {});
 
-        if (!res.ok) {
-            throw new Error("Failed to fetch visitors")
-        }
-        return res.json()
-    } catch (error) {
-        console.log("Error loading visitors", error);
-
+    if (!res.ok) {
+      throw new Error("Failed to fetch visitors");
     }
-}
+    return res.json();
+  } catch (error) {
+    console.log("Error loading visitors", error);
+  }
+};
 
-const VisitorCard = ({ name, surname, position, company, status, number, email, country, industry, state }: VisitorProps) => {
-    return (
-        <div className="my-6 border p-4 flex justify-between">
-            <div className="rounded flex gap-2">
-                <div className="border p-2 flex items-center justify-center rounded-full w-20 h-20">
-                    <MdPerson className='text-4xl text-slate-400' />
-                </div>
-                <div className="">
-                    <div className="flex gap-2 text-2xl font-bold">
-                        <p className="">{name}</p>
-                        <p className="">{surname}</p>
-                    </div>
-                    <div className="text-slate-500">
-                        <div className="flex items-center gap-2">
-                            <MdPerson className='text-slate-400' />
-                            <p className="">{position}</p>
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <MdHomeWork className='text-slate-400' />
-                            <p className="">{company}</p>
-                        </div>
-                    </div>
-                    <div className="flex gap-2 items-center text-slate-500">
-                        <MdEmail className='text-slate-400' />
-                        <p className="">{email}</p>
-                    </div>
-                    <div className="flex gap-2 items-center text-slate-500">
-                        <MdPhone className='text-slate-400' />
-                        <p className="">{number}</p>
-                    </div>
-                    <div className="flex gap-2 items-center text-slate-500">
-                        <MdLocationPin className='text-slate-400' />
-                        <p className="">{state}</p>
-                    </div>
-                    <div className="flex gap-2 items-center text-slate-500">
-                        <MdFlag className='text-slate-400' />
-                        <p className="">{country}</p>
-                    </div>
-                </div>
-            </div>
-            <div className="flex flex-col items-center justify-between">
-                <div className="space-y-2">
-                    <div className="p-4 h-6 bg-red-400 rounded flex items-center justify-center text-white text-sm">
-                        {status}
-                    </div>
-                    <div className="p-4 h-6 bg-blue-400 rounded flex items-center justify-center text-white text-sm">
-                        {status}
-                    </div>
-                </div>
-                <div className="flex items-center gap-2">
-                    <div className="border  h-8 w-8 text-slate-600 flex items-center justify-center rounded-full">
-                        <MdEdit />
-                    </div>
-                    <div className="bg-red-700 h-8 w-8 text-white flex items-center justify-center rounded-full">
-                        <MdDelete />
-                    </div>
-                </div>
-            </div>
+const VisitorCard = ({
+  name,
+  surname,
+  position,
+  company,
+  status,
+  number,
+  email,
+  country,
+  industry,
+  state,
+}: VisitorProps) => {
+  return (
+    <div className="my-6 border p-4 flex justify-between">
+      <div className="rounded flex gap-2">
+        <div className="border p-2 flex items-center justify-center rounded-full w-20 h-20">
+          <MdPerson className="text-4xl text-slate-400" />
         </div>
-    )
-}
+        <div className="">
+          <div className="flex gap-2 text-2xl font-bold">
+            <p className="">{name}</p>
+            <p className="">{surname}</p>
+          </div>
+          <div className="text-slate-500">
+            <div className="flex items-center gap-2">
+              <MdPerson className="text-slate-400" />
+              <p className="">{position}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <MdHomeWork className="text-slate-400" />
+              <p className="">{company}</p>
+            </div>
+          </div>
+          <div className="flex gap-2 items-center text-slate-500">
+            <MdEmail className="text-slate-400" />
+            <p className="">{email}</p>
+          </div>
+          <div className="flex gap-2 items-center text-slate-500">
+            <MdPhone className="text-slate-400" />
+            <p className="">{number}</p>
+          </div>
+          <div className="flex gap-2 items-center text-slate-500">
+            <MdLocationPin className="text-slate-400" />
+            <p className="">{state}</p>
+          </div>
+          <div className="flex gap-2 items-center text-slate-500">
+            <MdFlag className="text-slate-400" />
+            <p className="">{country}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-between">
+        <div className="space-y-2">
+          <div className="p-4 h-6 bg-red-400 rounded flex items-center justify-center text-white text-sm">
+            {status}
+          </div>
+          <div className="p-4 h-6 bg-blue-400 rounded flex items-center justify-center text-white text-sm">
+            {status}
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="border  h-8 w-8 text-slate-600 flex items-center justify-center rounded-full">
+            <MdEdit />
+          </div>
+          <div className="bg-red-700 h-8 w-8 text-white flex items-center justify-center rounded-full">
+            <MdDelete />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 async function Visitors() {
-    const { visitors } = await getVisitors()
-    let number = 0
-    return (
-        <div className=''>
-            <div className="">
-                {
-                    visitors.map((data: any, index: any) => {
-                        number = index
-                        return (
-                            <div key={index} className="hidden">{index}
-                            </div>
-                        )
-                    })
-                }
+  const { visitors } = await getVisitors();
+  let number = 0;
+  return (
+    <div className="">
+      <div className="">
+        {visitors.map((data: any, index: any) => {
+          number = index;
+          return (
+            <div key={index} className="hidden">
+              {index}
             </div>
-            <div className="font-bold text-lg">Visitors <span className=''>{`( ${number} )`}</span></div>
-            <div className="">
-                {
-                    visitors.map((visitor: any) => (
-                        <VisitorCard
-                            key={visitors._id}
-                            name={visitor.firstName}
-                            surname={visitor.lastName}
-                            position={visitor.position}
-                            company={visitor.company}
-                            status={'Pending'}
-                            number={visitor.phoneNumber}
-                            country={visitor.country}
-                            email={visitor.email}
-                            industry={visitor.industry}
-                            state={`Company`}
-                        />
-                    ))
-                }
-            </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+      <div className="font-bold text-lg">
+        Visitors <span className="">{`( ${number} )`}</span>
+      </div>
+      <div className="">
+        {visitors.map((visitor: any) => (
+          <VisitorCard
+            key={visitors._id}
+            name={visitor.firstName}
+            surname={visitor.lastName}
+            position={visitor.position}
+            company={visitor.company}
+            status={"Pending"}
+            number={visitor.phoneNumber}
+            country={visitor.country}
+            email={visitor.email}
+            industry={visitor.industry}
+            state={`Company`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Visitors
+export default Visitors;
