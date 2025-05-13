757c77af21ef08b730a08c8f333aed177501911c
Compulink-Dev <devcompulink@gmail.com>
2025-05-13 10:53:46 +0200

    speakers

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,48 +1,50 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { Colors } from '@/constant/colors';
-import Title from './NewTitle';
-
+import React, { useState } from "react";
+import { Colors } from "@/constant/colors";
+import Title from "./NewTitle";
 
 const Subscribe = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   const sendMail = async (e: any) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('/api/subscribe', {
-        method: 'POST',
+      const response = await fetch("/api/subscribe", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ email }),
       });
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const data = await response.json();
       console.log(data);
-      setEmail(''); // Clear the input on successful submission
+      setEmail(""); // Clear the input on successful submission
     } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
+      console.error("There was a problem with the fetch operation:", error);
     }
   };
 
   return (
     <div className="p-8 text-slate-400 bg-blue-950">
-      <div className="bg-blue-900 rounded p-8" style={{ background: Colors.primary }}>
+      <div
+        className="bg-blue-900 rounded p-8"
+        style={{ background: Colors.primary }}
+      >
         <div>
-          <Title
-            title="Stay Tunes with"
-            subtitle="weekly Newsletter"
-          />
+          <Title title="Stay Tunes with" subtitle="weekly Newsletter" />
         </div>
         <div className="px:0 md:px-32">
-          <form onSubmit={sendMail} className="flex flex-col gap-4 justify-center">
+          <form
+            onSubmit={sendMail}
+            className="flex flex-col gap-4 justify-center"
+          >
             <div className="bg-white flex items-center justify-between rounded p-1">
               <input
                 type="email"
