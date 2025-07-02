8f74bf2dd4f985c14a9f0647120ed6a89fbdbace
Compulink-Dev <devcompulink@gmail.com>
2025-07-02 09:45:42 +0200

    logos

---
+++ app/(admin)/dashboard/online-payment/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function OnlinePayment() {
+  return (
+    <div>
+      <p className="">Create Payment</p>
+      <div className="">
+        <a
+          href="https://www.paynow.co.zw/Payment/Link/?q=c2VhcmNoPWRpZ2l0YWxwYXltZW50cyU0MGNvbXB1bGluay5jby56dyZhbW91bnQ9OTYyLjYwJnJlZmVyZW5jZT0xNDY0NTYzMyZsPTE%3d"
+          target="_blank"
+        >
+          <img
+            src="https://www.paynow.co.zw/Content/Buttons/Medium_buttons/button_pay-now_medium.png"
+            style={{ border: 0 }}
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default OnlinePayment;
